ae76cc13dbaf9e54c06794e4f386b7b8b865c8d1
kiwdazueira69 <harrykewell2310@gmail.com>
2025-05-07 21:53:52 -0300

    Update webhook_perfectpay.py

--- webhook_perfectpay.py
+++ webhook_perfectpay.py
@@ -80,8 +80,17 @@ def pagamento_perfectpay():
 
 import os
 
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "🚀 API Flask está funcionando no Render!"
+
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
+
 
